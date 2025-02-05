addf247c3f8e43737b34db0fa31ad7d13ede28c9
Ethan Turnbull <ethanturnbull@outlook.com>
2025-02-05 14:55:41 +0000

    Change pinout to support new layout

---
+++ can-node.X/build/default/production/can_msgs.p1.d
@@ -0,0 +1,12 @@
+build/default/production/can_msgs.p1:  \
+can_msgs.c  \
+config.h  \
+mcc_generated_files/mcc.h  \
+mcc_generated_files/device_config.h  \
+mcc_generated_files/pin_manager.h  \
+mcc_generated_files/interrupt_manager.h  \
+mcc_generated_files/adc.h  \
+mcc_generated_files/clkref.h  \
+mcc_generated_files/can1.h  \
+mcc_generated_files/can_types.h  \
+mcc_generated_files/spi1.h 
--- can-node.X/main.c
+++ can-node.X/main.c
@@ -44,6 +44,7 @@
 #include "config.h"
 #include "can_msgs.h"
 #include "therm_LUT.h"
+#include "spi.h"
 
 ADC_channel_t therm_to_adc_channel[] = {
 THERM_1,
@@ -63,6 +64,13 @@ PD_9,
 PD_10
 };
 
+void SPI_SendString(const char *str)
+{
+    while(*str)
+    {
+        SPI1_ExchangeByte(*str++);
+    }
+}
 
 int8_t adc_to_temp(adc_result_t reading) 
 {
@@ -70,75 +78,93 @@ int8_t adc_to_temp(adc_result_t reading)
 	return therm_lut[reading];
 }
 
-uint8_t module_id; // 0 to 15, set by config resistors R3-R6
+uint8_t module_id; // 0 to 3, set by config resistors R5 and R6
 int8_t temps[THERM_COUNT];
 ADC_channel_t pd_voltages[PD_COUNT];
 
 void main(void)
 {
-    uint8_t is_dead;
-    CAN_MSG_OBJ msg;
-    
-    // initialise the device
-    SYSTEM_Initialize();
-	ADC_SelectContext(CONTEXT_1);
-    
+  uint8_t is_dead;
+  CAN_MSG_OBJ msg;
+  
+  // initialise the device
+  SYSTEM_Initialize();
+  SPI1_Initialize();
+ADC_SelectContext(CONTEXT_1);
+  
 //     read module ID from config resistors
-    module_id = (1 - CFG_R5_GetValue()) +
-                (1 - GFG_R6_GetValue()) * 2;
-        
-    while (1)
-    {   
-        //read temps
-		for (uint8_t therm_i = MIN_THERM_ID; therm_i <= MAX_THERM_ID; therm_i++)
-		{
-      ADC_StartConversion(therm_to_adc_channel[therm_i]);
-      while(!ADC_IsConversionDone());
-      adc_result_t reading = ADC_GetConversionResult();
-      temps[therm_i-MIN_THERM_ID] = adc_to_temp(reading);
-		}
+  module_id = (1 - CFG_R5_GetValue()) +
+              (1 - GFG_R6_GetValue()) * 2;
+      
+  while (1)
+  {   
+      //read temps
+  for (uint8_t therm_i = MIN_THERM_ID; therm_i <= MAX_THERM_ID; therm_i++)
+  {
+    ADC_StartConversion(therm_to_adc_channel[therm_i]);
+    while(!ADC_IsConversionDone());
+    adc_result_t reading = ADC_GetConversionResult();
+    temps[therm_i-MIN_THERM_ID] = adc_to_temp(reading);
+    
+      char buffer[50];
+      sprintf(buffer, "Temp[%d]: %d\n", therm_i, temps[therm_i - MIN_THERM_ID]);
+      SPI_SendString(buffer); // Send temperature values over SPI
+  }
 
-    for (uint8_t pd_i = MIN_PD_ID; pd_i <= MAX_PD_ID; pd_i++)
-    {
-      ADC_StartConversion(pd_to_adc_channel[pd_i]);
-      while(!ADC_IsConversionDone());
-      adc_result_t reading = ADC_GetConversionResult();
-      // The following is implemented into the candbc file
-      //reading = reading * 3.3 * 5 / 1024; // convert to volts (3.3v reference, 4:1 potential divider and 12 bit adc)
-      pd_voltages[pd_i-MIN_PD_ID] = reading;
-    }
+  for (uint8_t pd_i = MIN_PD_ID; pd_i <= MAX_PD_ID; pd_i++)
+  {
+    ADC_StartConversion(pd_to_adc_channel[pd_i]);
+    while(!ADC_IsConversionDone());
+    adc_result_t reading = ADC_GetConversionResult();
+    // The following is implemented into the candbc file
+    //reading = reading * 3.3 * 5 / 1024; // convert to volts (3.3v reference, 4:1 potential divider and 12 bit adc)
+    pd_voltages[pd_i-MIN_PD_ID] = reading;
+
+      char buffer[50];
+      sprintf(buffer, "PD Voltage[%d]: %d\n", pd_i, pd_voltages[pd_i - MIN_PD_ID]);
+      SPI_SendString(buffer); // Send voltage values over SPI
+  }
 
 
-        // set up can interface
-		switch (CAN1_OperationModeGet())
-		{
-		case CAN_CONFIGURATION_MODE:
-			CAN1_OperationModeSet(CAN_NORMAL_2_0_MODE);
-			break;
-		case CAN_NORMAL_2_0_MODE:
-			// transmit data
-		    if(CAN_TX_FIFO_AVAILABLE == (CAN1_TransmitFIFOStatusGet(CAN1_TX_TXQ)))
-		    {
-          // PD broadcast msg
-          for (uint8_t multiplexor = 0; multiplexor <= 1; multiplexor++)
-          {
-            msg = get_PD_Broadcast_msg(pd_voltages, module_id, multiplexor);
-            CAN1_Transmit(CAN1_TX_TXQ, &msg);
-            while(CAN1_TransmitFIFOStatusGet(CAN1_TX_TXQ) == CAN_TX_FIFO_FULL);
-          }
-
-          // Thermistor broadcast msg
-          uint8_t multiplexor = 3;
-          msg = get_Therm_Broadcast_msg(temps, module_id, multiplexor);
+      // set up can interface
+  switch (CAN1_OperationModeGet())
+  {
+  case CAN_CONFIGURATION_MODE:
+    CAN1_OperationModeSet(CAN_NORMAL_2_0_MODE);
+    break;
+  case CAN_NORMAL_2_0_MODE:
+    // transmit data
+      if(CAN_TX_FIFO_AVAILABLE == (CAN1_TransmitFIFOStatusGet(CAN1_TX_TXQ)))
+      {
+        // PD broadcast msg
+        for (uint8_t multiplexor = 0; multiplexor <= 1; multiplexor++)
+        {
+          msg = get_PD_Broadcast_msg(pd_voltages, module_id, multiplexor);
           CAN1_Transmit(CAN1_TX_TXQ, &msg);
           while(CAN1_TransmitFIFOStatusGet(CAN1_TX_TXQ) == CAN_TX_FIFO_FULL);
-		    }
-			break;
-		default:
-			break;
-		}
-		__delay_ms(LOOP_PERIOD);
-    }
+
+            char buffer[50];
+            sprintf(buffer, "PD Broadcast Msg: Multiplexor %d\n", multiplexor);
+            SPI_SendString(buffer); // Send PD broadcast message over SPI
+        }
+
+        // Thermistor broadcast msg
+        uint8_t multiplexor = 2;
+        msg = get_Therm_Broadcast_msg(temps, module_id, multiplexor);
+        CAN1_Transmit(CAN1_TX_TXQ, &msg);
+        while(CAN1_TransmitFIFOStatusGet(CAN1_TX_TXQ) == CAN_TX_FIFO_FULL);
+          
+          char buffer[50];
+          sprintf(buffer, "Thermistor Broadcast Msg");
+          SPI_SendString(buffer); // Send Thermistor broadcast message over SPI
+        }
+      }
+    break;
+  default:
+    break;
+  }
+  __delay_ms(LOOP_PERIOD);
+  }
 }
 /**
  End of File
